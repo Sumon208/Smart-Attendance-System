f6f3047559a7fa7d3846a18f88b264972be25b45
Sumon Roy <142534892+Sumon208@users.noreply.github.com>
2025-08-19 13:22:30 +0600

    commit

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -32,6 +32,9 @@ namespace Smart_Attendance_System.Models
         public DateTime? DateOfBirth { get; set; }
 
         public string? Gender { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
+        public string? JoiningDate { get; set; }
 
         public int? DepartmentId { get; set; }
 
